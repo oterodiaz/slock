27cd8cd82055ec66c98c7049de94f2338c3ac780
Otero Díaz <4396445-oterodiaz@users.noreply.gitlab.com>
2020-12-16 22:58:28 +0100

    Apply patch: dpms

--- config.def.h
+++ config.def.h
@@ -10,6 +10,9 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+ 
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
 
 /* default message */
 static const char * message = "Suckless: Software that sucks less.";
